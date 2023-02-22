1430b288ef48f8f320770e3229df211190616069
vgarshin <vgarshin@yandex.ru>
2023-02-22 13:17:06 +0000

    bugs

--- topic_1/lect_1_jhub_data_etl.sh
+++ topic_1/lect_1_jhub_data_etl.sh
@@ -6,19 +6,19 @@ echo ${file_path}
 
 python lect_1_jhub_data_processing.py ${file_path}
 
-database=jhub2
+database=jhub3
 
-# Users
+# users
 
 psql -c 'create database '${database}';'
 psql -d ${database} -c 'CREATE TABLE users (login varchar(32) NOT NULL, name varchar(128), email varchar(128));'
 psql -d ${database} \
-    -c "COPY users FROM '/home/jovyan/ibdt_course_miba_23/users.csv' \
+    -c "COPY users FROM '/home/jovyan/ibdt_course_miba_23/topic_1/users.csv' \
     DELIMITER ';' CSV HEADER"
 
 # Logs
 
 psql -d ${database} -c 'CREATE TABLE logs (idx bigint primary key, timestamp timestamp, hub varchar(20), event_code bigint, event_type varchar(32), log varchar(512), login varchar(32));'
 psql -d ${database}  \
-    -c "COPY logs FROM '/home/jovyan/ibdt_course_miba_23/logs.csv' \
+    -c "COPY logs FROM '/home/jovyan/ibdt_course_miba_23/topic_1/logs.csv' \
     DELIMITER ';' CSV HEADER"
